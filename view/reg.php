42f32efc464e8abbed4440ca5b1c91d605b21e53
unknown <vlad.bas.55@mial.ru>
2024-03-04 11:37:30 +0300

    asd

--- view/reg.php
+++ view/reg.php
@@ -2,12 +2,20 @@
 include_once "component/start.php";
 
 ?>
-<div class="register">
-  <form method="post" action="">
-     <input type="text" name="login">
-     <input type="text" name="password">
-     <button type="submit">Зарегистрироваться</button>
-  </form>
+<div class="register col-3">
+<form method="post" action="">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
   </div>
   <?php
 include_once "component/end.php";
